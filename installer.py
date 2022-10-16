fc7e4da8871fa364520d8f188ea34f7a65289d1d
Letder40 <74748354+Letder40@users.noreply.github.com>
2022-10-16 18:29:36 +0000

    I forgot to update the installer XD

--- installer.py
+++ installer.py
@@ -1,4 +1,16 @@
 import os
+import platform
+
+sistema = platform.system()
+
+if sistema == "Darwin":
+    os.system("curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py")
+    os.system("python3 get-pip.py")
+    os.system("rm get-pip.py")
+    os.system("python3 -m pip install colorama")
+    input("All went without any problem, thank you so much for install pyzzer, enjoy :D")
+    exit(0)
+    
 try:
     os.system("pip install colorama")
     input("All went without any problem, thank you so much for install pyzzer, enjoy :D")
